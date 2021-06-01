37f029e73f6d85a622401a1c3561e10afaf2ee00
Bartek Wieckowski <bart.it.pro@gmail.com>
2021-06-01 17:55:32 +0200

    add and remove classList from error action

--- main.js
+++ main.js
@@ -11,3 +11,29 @@ const emailInput = emailField.querySelector("input");
 
 const passwordField = document.querySelector(".password");
 const passwordInput = passwordField.querySelector("input");
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  if (firstNInput.value == "") {
+    firstNField.classList.add("shake", "error");
+  }
+  if (lastNInput.value == "") {
+    lastNField.classList.add("shake", "error");
+  }
+  if (passwordInput.value == "") {
+    passwordField.classList.add("shake", "error");
+  }
+  if (emailInput.value == "") {
+    emailField.classList.add("shake", "error");
+  } else {
+    checkEmail();
+  }
+
+  setTimeout(() => {
+    firstNField.classList.remove("shake");
+    lastNField.classList.remove("shake");
+    emailField.classList.remove("shake");
+    passwordField.classList.remove("shake");
+  }, 400);
+};
