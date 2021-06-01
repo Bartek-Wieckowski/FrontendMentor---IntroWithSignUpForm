3ca8e9a20b2827e5d3e709b8dbc4c55337294b3d
Bartek Wieckowski <bart.it.pro@gmail.com>
2021-06-01 17:47:40 +0200

    querySelector for variables

---
+++ main.js
@@ -0,0 +1,13 @@
+const form = document.querySelector(".form");
+
+const firstNField = document.querySelector(".first-name");
+const firstNInput = firstNField.querySelector("input");
+
+const lastNField = document.querySelector(".last-name");
+const lastNInput = lastNField.querySelector("input");
+
+const emailField = document.querySelector(".email");
+const emailInput = emailField.querySelector("input");
+
+const passwordField = document.querySelector(".password");
+const passwordInput = passwordField.querySelector("input");
